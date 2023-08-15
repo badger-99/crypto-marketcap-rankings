778dfb98cac0204a27f812e7ac2476c8d99cec45
Alfred Peter <alpeter91@gmail.com>
2023-08-15 15:42:52 +0300

    Update test for navigtion component

--- src/components/Navigation.test.js
+++ src/components/Navigation.test.js
@@ -1,13 +1,31 @@
 import { MemoryRouter } from 'react-router';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import { render } from '@testing-library/react';
 import Navigation from './Navigation';
 
+const mockStore = configureMockStore();
+
 test('Navigation component renders as expected', () => {
-  const navigation = render(
+  const initialState = {
+    crypto: {
+      cryptoArray: [
+        {name:'testCoin',}
+      ]
+    }
+  }
+
+  const store = mockStore(initialState);
+
+  const {getByRole, getByText} = render(
     <MemoryRouter>
-      <Navigation />
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
     </MemoryRouter>,
   );
 
-  expect(navigation).toMatchSnapshot();
+  const navLink = getByRole('link')
+  expect(getByText('#1 testCoin')).toBeInTheDocument();
+  expect(navLink.getAttribute('href')).toBe('/1');
 });

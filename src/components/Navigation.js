4a94578869f3996186f0f094fc273962bba9d643
Alfred Peter <alpeter91@gmail.com>
2023-08-15 18:10:34 +0300

    Implement search feature

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,55 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   const { cryptoArray } = useSelector((store) => store.crypto);
+  const [querry, setQuerry] = useState('');
 
   const generateNavLinks = (cryptoArray) => {
     const linkArray = [];
     for (let rank = 1; rank <= cryptoArray.length; rank += 1) {
       linkArray.push(
         <NavLink key={rank} className="link" to={`/${rank}`}>
-          {`#${rank} ${cryptoArray[rank - 1].name}`}
+          {`#${cryptoArray[rank - 1].rank} ${cryptoArray[rank - 1].name}`}
         </NavLink>,
       );
     }
     return linkArray;
   };
 
+  const searchByRank = (rank) => {
+    const token = cryptoArray.filter((coin) => coin.rank === rank);
+
+    if (token.length === 0) {
+      return <p>Out Of Bounds.</p>;
+    }
+
+    let result = [];
+    result = generateNavLinks(token);
+    return result;
+  };
+
   return (
     <nav>
-      <ul className="column">
-        {generateNavLinks(cryptoArray)}
-      </ul>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      >
+        <input type="text" placeholder="Search by Rank" onChange={(e) => setQuerry(e.target.value)} />
+        <button type="submit">Serach</button>
+      </form>
+      {querry
+        ? (
+          <div>
+            {searchByRank(querry)}
+          </div>
+        )
+        : (
+          <ul className="column">
+            {generateNavLinks(cryptoArray)}
+          </ul>
+        )}
     </nav>
   );
 };

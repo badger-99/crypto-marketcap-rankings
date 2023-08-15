fea8b0b0746d36f856028fabe6329bc6ddc5e313
Alfred Peter <alpeter91@gmail.com>
2023-08-15 20:05:29 +0300

    Update Home component snpshot test

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,11 +1,15 @@
 import { MemoryRouter } from 'react-router';
+import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
+import store from '../redux/store'
 import Home from './Home';
 
 test('Home component renders as expected', () => {
   const home = render(
     <MemoryRouter>
-      <Home />
+      <Provider store={store}>
+        <Home />
+      </Provider>
     </MemoryRouter>,
   );
 

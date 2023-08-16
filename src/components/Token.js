a4f3cc9766a3aa95a238abcf4d403dbbd6bcee37
Alfred Peter <alpeter91@gmail.com>
2023-08-16 13:03:45 +0300

    Remove error display from Token

--- src/components/Token.js
+++ src/components/Token.js
@@ -3,11 +3,7 @@ import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Token = ({ rank }) => {
-  const { cryptoArray, error } = useSelector((store) => store.crypto);
-
-  if (error) {
-    return <h1>{error}</h1>;
-  }
+  const { cryptoArray } = useSelector((store) => store.crypto);
 
   const token = cryptoArray.filter((coin) => coin.rank === rank)[0];
 
@@ -19,10 +15,22 @@ const Token = ({ rank }) => {
       <div>
         <h1>{`#${token.rank}`}</h1>
         <h2>{`Name: ${token.name} (${token.symbol})`}</h2>
-        <h4>{`Price: $ ${token.priceUsd}`}</h4>
-        <h4>{`Supply: ${token.supply} units`}</h4>
-        <h4>{`Max-supply: ${token.maxSupply} units`}</h4>
-        <h4>{`Market Cap: $ ${token.marketCapUsd}`}</h4>
+        <p>
+          Market Cap:
+          <span>{`$ ${token.marketCapUsd}`}</span>
+        </p>
+        <p>
+          Price:
+          <span>{`$ ${token.priceUsd}`}</span>
+        </p>
+        <p>
+          Supply:
+          <span>{`${token.supply} units`}</span>
+        </p>
+        <p>
+          Max-supply:
+          <span>{`${token.maxSupply} units`}</span>
+        </p>
       </div>
     </article>
   );

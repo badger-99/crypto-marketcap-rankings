103478f9cce7fbca816ea387f54d84552026eac7
Alfred Peter <alpeter91@gmail.com>
2023-08-16 13:04:39 +0300

    Test error displayin Navigation component

--- src/components/Navigation.test.js
+++ src/components/Navigation.test.js
@@ -9,29 +9,58 @@ import Navigation from './Navigation';
 
 const mockStore = configureMockStore();
 
-test('Navigation component renders as expected', () => {
-  const initialState = {
-    crypto: {
-      cryptoArray: [
-        {
-          name: 'testCoin',
-          rank: '1',
-        },
-      ],
-    },
-  };
+describe('test Navigation component', () => {
+  test('Navigation component renders as expected', () => {
+    const initialState = {
+      crypto: {
+        cryptoArray: [
+          {
+            name: 'testCoin',
+            rank: '1',
+          },
+        ],
+        error: null,
+      },
+    };
 
-  const store = mockStore(initialState);
+    const store = mockStore(initialState);
 
-  const { getByRole, getByText } = render(
-    <MemoryRouter>
-      <Provider store={store}>
-        <Navigation />
-      </Provider>
-    </MemoryRouter>,
-  );
+    const { getByRole, getByText } = render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Navigation />
+        </Provider>
+      </MemoryRouter>
+    );
 
-  const navLink = getByRole('link');
-  expect(getByText('#1 testCoin')).toBeInTheDocument();
-  expect(navLink.getAttribute('href')).toBe('/1');
+    const navLink = getByRole('link');
+    expect(getByText('#1 testCoin')).toBeInTheDocument();
+    expect(navLink.getAttribute('href')).toBe('/1');
+  });
+
+  it('renders an error message after a failed API request', () => {
+    const initialState = {
+      crypto: {
+        cryptoArray: [
+          {
+            name: 'testCoin',
+            rank: '1',
+          },
+        ],
+        error: 'There has been an error',
+      },
+    };
+
+    const store = mockStore(initialState);
+
+    const { getByText } = render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Navigation />
+        </Provider>
+      </MemoryRouter>
+    );
+
+    expect(getByText('There has been an error')).toBeInTheDocument();
+  });
 });

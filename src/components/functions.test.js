4039b0fac73ccbdccb981d1025c9730cd22a45c5
Alfred Peter <alpeter91@gmail.com>
2023-08-16 03:56:44 +0300

    Test generateNavLinks function

---
+++ src/components/functions.test.js
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { generateNavLinks, searchByRank } from './functions';
+
+describe('testing helper functions', () => {
+  test('test generateNavLinks function', () => {
+    const cryptoArray = [
+      {
+        name: 'testCoin',
+        rank: '1',
+      },
+      {
+        name: 'subCoin',
+        rank: '2',
+      },
+    ];
+
+    const container = render(
+      <MemoryRouter>
+        <ul>{generateNavLinks(cryptoArray)}</ul>
+      </MemoryRouter>
+    );
+
+    const li1 = screen.getByText('#1 testCoin')
+    const li2 = screen.getByText('#2 subCoin')
+
+    expect(li1).toBeInTheDocument();
+    expect(li2).toBeInTheDocument();
+  });
+});

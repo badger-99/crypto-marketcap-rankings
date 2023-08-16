392c4dc3ff641ef2a1e388ae3e678a5333abdd4b
Alfred Peter <alpeter91@gmail.com>
2023-08-16 16:50:39 +0300

    Add loading spinner

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import ClipLoader from 'react-spinners/ClipLoader';
 import { generateNavLinks, searchByRank } from './functions';
 
 const Navigation = () => {
-  const { cryptoArray, error } = useSelector((store) => store.crypto);
+  const { cryptoArray, isLoading, error } = useSelector((store) => store.crypto);
   const [querry, setQuerry] = useState('');
+
+  if (isLoading) {
+    return (
+      <div>
+        <ClipLoader loading color="blue" size={32} speedMultiplier={1} />
+      </div>
+    );
+  }
 
   if (error) {
     return <div><h1>{error}</h1></div>;
@@ -18,11 +27,11 @@ const Navigation = () => {
         }}
       >
         <input
-          type='text'
-          placeholder='Search by Rank'
+          type="text"
+          placeholder="Search by Rank"
           onChange={(e) => setQuerry(e.target.value)}
         />
-        <button type='submit'>Search</button>
+        <button type="submit">Search</button>
       </form>
       {querry ? (
         <div>{searchByRank(cryptoArray, querry)}</div>

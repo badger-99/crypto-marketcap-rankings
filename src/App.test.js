48ac74c696ccfb5c1f8d08bd4444291f056ab291
Alfred Peter <alpeter91@gmail.com>
2023-08-16 12:14:35 +0300

    Add App component snapshot test

---
+++ src/App.test.js
@@ -0,0 +1,42 @@
+import { MemoryRouter } from "react-router";
+import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import { render } from "@testing-library/react";
+import App from "./App";
+
+const mockStore = configureMockStore([thunk])
+
+test('App component renders as expected', () => {
+
+  const initialState = {
+    crypto: {
+      cryptoArray: [
+        {
+          name: 'testCoin',
+          rank: '1',
+        },
+        {
+          name: 'subCoin',
+          rank: '2',
+        },
+        {
+          name: 'miniCoin',
+          rank: '3',
+        },
+      ],
+    },
+  };
+
+  const store = mockStore(initialState)
+
+  const app = render(
+    <MemoryRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MemoryRouter>
+  )
+
+  expect(app).toMatchSnapshot();
+})

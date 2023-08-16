9a20e868455eca7a21941c3b972baa96ed7daa07
Alfred Peter <alpeter91@gmail.com>
2023-08-16 20:54:18 +0300

    Add current date

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,27 @@
+import { DateTime } from 'luxon';
 import { SiCoinmarketcap } from 'react-icons/si';
 import { BsFillMicFill } from 'react-icons/bs';
 import { IoSettingsSharp } from 'react-icons/io5';
 import Navigation from './Navigation';
 
+const dt = DateTime.now();
+const date = dt.toLocaleString(DateTime.DATE_MED);
 const Home = () => (
   <div>
     <header className="row">
-      <div className="column">
-        <span>Coin</span>
-        <span>Market</span>
-        <span>Cap</span>
+      <div>
+        {date}
       </div>
-      <div><SiCoinmarketcap /></div>
+      <p>Rankings</p>
       <div>
         <BsFillMicFill />
         <IoSettingsSharp />
       </div>
     </header>
-    <h1>Top-10 Crypto Currencies</h1>
+    <div className="row">
+      <SiCoinmarketcap />
+      <h1>Top-10 Crypto Currencies</h1>
+    </div>
     <Navigation />
   </div>
 );

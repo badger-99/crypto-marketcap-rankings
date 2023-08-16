42b7ff8cac2810071880149113f346ecbd59ed3f
Alfred Peter <alpeter91@gmail.com>
2023-08-16 13:04:00 +0300

    Update Token component test

--- src/components/Token.test.js
+++ src/components/Token.test.js
@@ -7,41 +7,7 @@ import Token from './Token';
 const mockStore = configureMockStore();
 
 describe('testing the Token component', () => {
-  it('renders an error message after a failed API request', () => {
-    const initialState = {
-      crypto: {
-        cryptoArray: [
-          {
-            id: 'polkadot',
-            rank: '11',
-            symbol: 'DOT',
-            name: 'Polkadot',
-            supply: '1264429412.9423500000000000',
-            maxSupply: null,
-            marketCapUsd: '6284893105.6033794423726904',
-            volumeUsd24Hr: '33786670.5474617752250812',
-            priceUsd: '4.9705369404357022',
-            changePercent24Hr: '-0.4536725529741895',
-            vwap24Hr: '4.9889329299971268',
-            explorer: 'https://polkascan.io/polkadot',
-          },
-        ],
-        error: 'There has been an error',
-      },
-    };
-
-    const store = mockStore(initialState);
-
-    const token = render(
-      <Provider store={store}>
-        <Token rank="11" />
-      </Provider>,
-    );
-
-    expect(token.getByText('There has been an error')).toBeInTheDocument();
-  });
-
-  it('correctly renders the link to the home page', () => {
+  test('correctly renders the link to the home page', () => {
     const initialState = {
       crypto: {
         cryptoArray: [
@@ -80,7 +46,7 @@ describe('testing the Token component', () => {
     expect(homeLink.getAttribute('href')).toBe('/');
   });
 
-  it('renders correct cryptocurrency detais', () => {
+  test('renders correct cryptocurrency detais', () => {
     const initialState = {
       crypto: {
         cryptoArray: [
@@ -115,9 +81,13 @@ describe('testing the Token component', () => {
 
     expect(token.getByText('#11')).toBeInTheDocument();
     expect(token.getByText('Name: Polkadot (DOT)')).toBeInTheDocument();
-    expect(token.getByText('Price: $ 4.97')).toBeInTheDocument();
-    expect(token.getByText('Supply: 1264429412.94 units')).toBeInTheDocument();
-    expect(token.getByText('Max-supply: Unlimited units')).toBeInTheDocument();
-    expect(token.getByText('Market Cap: $ 6284893105.60')).toBeInTheDocument();
+    expect(token.getByText('Price:')).toBeInTheDocument();
+    expect(token.getByText('$ 4.97')).toBeInTheDocument();
+    expect(token.getByText('Supply:')).toBeInTheDocument();
+    expect(token.getByText('1264429412.94 units')).toBeInTheDocument();
+    expect(token.getByText('Max-supply:')).toBeInTheDocument();
+    expect(token.getByText('Unlimited units')).toBeInTheDocument();
+    expect(token.getByText('Market Cap:')).toBeInTheDocument();
+    expect(token.getByText('$ 6284893105.60')).toBeInTheDocument();
   });
 });

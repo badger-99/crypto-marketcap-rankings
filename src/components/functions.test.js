b25fad7e584348e36c79dfd319de59fef1d426e4
Alfred Peter <alpeter91@gmail.com>
2023-08-17 19:03:03 +0300

    Fix linter errors

--- src/components/functions.test.js
+++ src/components/functions.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router';
 import { generateNavLinks, generateRoutes, searchByRank } from './functions';
 
